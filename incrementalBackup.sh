1d3dde4529839517e9e1b80aa6c0c77d472a8721
moltubaer <alexandermoltu@gmail.com>
2022-04-12 20:47:15 +0200

    Lagt til SCP i incrementalBackup.sh

--- incrementalBackup.sh
+++ incrementalBackup.sh
@@ -22,9 +22,6 @@ function incrementalBackup () {
     for (( i=0; i<${#locations[@]}; i++ )); do
         base=$(basename ${locations[$i]})
         if [[ -d ${locations[$i]} ]]; then
-            base=$(basename ${locations[$i]})
-            backupdest="$dest/$base"
-            # mkdir -p $backupdest
             find ${locations[$i]}/* -mmin -60 -exec cp -rp --parents "{}"  $dest \;
         elif [[ -f ${locations[$i]} ]]; then
             find ${locations[$i]} -maxdepth 0 -type f -mmin -60 -exec cp -rp --parents "{}" $dest \;
@@ -53,9 +50,6 @@ function fullBackup () {
     for (( i=0; i<${#locations[@]}; i++ )); do
         base=$(basename ${locations[$i]})
         if [[ -d ${locations[$i]} ]]; then
-            base=$(basename ${locations[$i]})
-            backupdest="$dest/$base"
-            # mkdir -p $backupdest
             find ${locations[$i]}/* -exec cp -rp --parents "{}"  $dest \;
         elif [[ -f ${locations[$i]} ]]; then
             find ${locations[$i]} -maxdepth 0 -type f -exec cp -rp --parents "{}" $dest \;
@@ -70,6 +64,14 @@ function fullBackup () {
     pg_dump -U $PGUSER $PGDATABASE > "$dest/database-production-backup.sql"
 }
 
+# Variabler for SCP
+
+
+dingsebomsHack="hackerspace@129.241.106.24"
+duppedittHack="hackerspace@129.241.106.25"
+# localBackup="/home/hackerspace/hackerspace-backups/$HOSTNAME-backup"
+fullzip="$(dirname $dest)/full-backup$(date +"%d-%m-%y").zip"
+inczip="$(dirname $dest)/inc-backup$(date +"%d-%m-%y").zip"
 
 today="$(date +%A)"
 # backupday="Sunday"
@@ -80,9 +82,17 @@ if [ "$today" == "$backupday" ]; then
     full="/home/hackerspace/hackerspace-backups/$HOSTNAME-backup/full-backup$(date +"%d-%m-%y")"
     fullzip="/home/hackerspace/hackerspace-backups/$HOSTNAME-backup/full-backup$(date +"%d-%m-%y").zip"
 
+    # Zipper backup og sletter den gamle
     if [ $(find $full -maxdepth 0 -type d) ]; then
         cd $(dirname $full); zip -r $(basename $full) .
         rm -rf $full
+    fi
+
+    # SCP til andre serveren i miljøet. Sjekker først hvem som er hvem
+    if [ "$HOSTNAME" == "Dingseboms" ]; then
+        scp $fullzip $duppedittHack:$(dirname $dest)
+    elif [ "$HOSTNAME" == "Duppeditt" ]; then
+        scp $fullzip $dingsebomsHack:$(dirname $dest)
     fi
 
 else 
@@ -91,11 +101,18 @@ else
     inc="/home/hackerspace/hackerspace-backups/$HOSTNAME-backup/inc-backup$(date +"%d-%m-%y")"
     inczip="/home/hackerspace/hackerspace-backups/$HOSTNAME-backup/inc-backup$(date +"%d-%m-%y").zip"
 
+    # Zipper backup og sletter den gamle
     if [ $(find $inc -maxdepth 0 -type d) ]; then
         cd $(dirname $inc); zip -r $(basename $inc) .
         rm -rf $inc
     fi
 
+    # SCP til andre serveren i miljøet. Sjekker først hvem som er hvem
+    if [ "$HOSTNAME" == "Dingseboms" ]; then
+        scp $inczip $duppedittHack:$(dirname $dest)
+    elif [ "$HOSTNAME" == "Duppeditt" ]; then
+        scp $inczip $dingsebomsHack:$(dirname $dest)
+    fi
 fi
 
 
